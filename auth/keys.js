905cb65d9f5b27ad7a734643fae201c8bcba7f8a
Michał Marchewczyk <dev@marchewczyk.eu>
2020-06-29 10:56:29 +0200

    Updated keys.js

--- auth/keys.js
+++ auth/keys.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const privateKey = fs.readFileSync(path.join(__dirname, 'gethido_rsa.key'));
-const publicKey = fs.readFileSync(path.join(__dirname, 'gethido_rsa.key'));
+const publicKey = fs.readFileSync(path.join(__dirname, 'gethido_rsa.pub.key'));
 
 module.exports = {
     privateKey: privateKey,
